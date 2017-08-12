419ead0eb395a25d4e3b04483eccd95387c668e2
Clément Louarn <c2hbahamut0@gmail.com>
2017-08-13 00:46:35 +0300

    forgot to add the js bit

--- js/grayscale.js
+++ js/grayscale.js
@@ -189,3 +189,20 @@ function init() {
         //icon: image
     });
 }
+
+
+$('.with-caption').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		zoom: {
+			enabled: true
+		}
+	});
