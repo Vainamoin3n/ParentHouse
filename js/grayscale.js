83ea00794029df33abe6efed8a20fc3c95a8e2fc
Clément Louarn <c2hbahamut0@gmail.com>
2017-08-13 20:21:18 +0300

    fixed a lot of issues

--- js/grayscale.js
+++ js/grayscale.js
@@ -191,18 +191,12 @@ function init() {
 }
 
 
-$('.with-caption').magnificPopup({
+$('.without-caption').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
 		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		mainClass: '', // class to remove default margin from left and right side
 		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
-		},
-		zoom: {
-			enabled: true
+			verticalFit: true
 		}
 	});
